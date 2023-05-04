b3f1db0758a63a06f5eaa1c5361e54017534830b
Rogelio Perez-Montero <131227318+RogePM@users.noreply.github.com>
2023-05-04 15:00:45 -0400

    new

--- src/main/java/edu/guilford/CalPane.java
+++ src/main/java/edu/guilford/CalPane.java
@@ -1,6 +1,5 @@
 package edu.guilford;
 
-
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -16,45 +15,39 @@ public class CalPane extends GridPane {
     private Button calculateButton;
     private ImageView imageview;
     private Calculate calculate;
-    //private final List<Calculate> calculationList;
-
+    
+    private Label billLabel;
+    private Label tipLabel;
+    private Label totalLabel;
 
     // create constructor for the calPane
-    public CalPane(Calculate calculate) {
+    public CalPane(Calculate calculate) throws NumberFormatException {
         super();
         this.calculate = calculate;
-        
-        // instantiate text fields 
+
+        // instantiate text fields
         billField = new TextField();
         tipField = new TextField();
         totalField = new TextField();
         calculateButton = new Button("Calculate");
 
-
         // add labels to the pane
-        Label billLabel = new Label("Bill Amount:");
+        billLabel = new Label("Bill Amount:");
         add(billLabel, 1, 0);
+        // add labels to the pane
+        tipLabel = new Label("Tip Percent:");
+        add(tipLabel, 1, 2);
+        totalLabel = new Label("Total Amount:");
+        add(totalLabel, 1, 3);
 
-        // add textfield to the pane
+        // add textfields to the pane
         billField = new TextField();
         billField.setPromptText("Enter bill amount");
         add(billField, 2, 0);
-        //put user input into the get bill method
-        
-
-        // add labels to the pane
-        Label tipLabel = new Label("Tip Percent:");
-        add(tipLabel, 1, 2);
-       
 
         tipField = new TextField();
         tipField.setPromptText("Enter tip percentage");
-        
         add(tipField, 2, 2);
-
-        // add labels to the pane
-        Label totalLabel = new Label("Total Amount:");
-        add(totalLabel, 1, 3);
 
         totalField = new TextField();
         totalField.setPromptText("total amount");
@@ -66,7 +59,16 @@ public class CalPane extends GridPane {
         add(calculateButton, 2, 4);
 
         // add event handler to the button
-        calculateButton.setOnAction(event -> calculateTip());
+        calculateButton.setOnAction(event -> {
+            try {
+                calculateTip();
+            } catch (NumberFormatException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                // display error in bill field
+                billLabel.setText("Error: Enter a numeric value");
+            }
+        });
 
         // set font of labels bigger
         billLabel.setStyle("-fx-font-size: 20px;");
@@ -82,48 +84,35 @@ public class CalPane extends GridPane {
 
         // event handler for tip percentage input
         tipField.textProperty().addListener((observable, oldValue, newValue) -> {
-            try {
-                double tipPercentage = Double.parseDouble(newValue);
-                if (tipPercentage < 20) {
-                    tipLabel.setTextFill(Color.RED);
-                    tipLabel.setText("Tip Percent:\nTip should be at least 20%)");
-                } else if (tipPercentage > 20) {
-                    tipLabel.setTextFill(Color.GREEN);
-                    tipLabel.setText("Tip Percent: " + (calculate.getTip()));
-                } else
-                    tipLabel.setTextFill(Color.BLACK);
-            } catch (NumberFormatException e) {
-
-                //
-            }
+            double tipPercentage = Double.parseDouble(newValue);
+            if (tipPercentage < 20) {
+                tipLabel.setTextFill(Color.RED);
+                tipLabel.setText("Tip Percent:\nTip should be at least 20%)");
+            } else if (tipPercentage > 20) {
+                tipLabel.setTextFill(Color.GREEN);
+                tipLabel.setText("Tip Percent:" + (calculate.getTip()));
+            } else
+                tipLabel.setTextFill(Color.BLACK);
         });
 
     }
 
     // method that calculates the tip
-    private void calculateTip() {
+    private void calculateTip() throws NumberFormatException {
         try {
-            // get the bill amount
-           // double billAmount = calculate.getBill();             //old code didn't work
-            //calculate.setBill(Double.parseDouble(billField.getText()));
-            //double tipPercentage = calculate.getTip();  //didn;t work
-            //calculate.setTip(Double.parseDouble(tipField.getText()));
-            //double tipPercentage = Double.parseDouble(tipField.getText());
-            
-          
+
             double billAmount = Double.parseDouble(billField.getText());
             System.out.println(billAmount);
             double tipPercentage = Double.parseDouble(tipField.getText());
             System.out.println(tipPercentage);
             double tipAmount = billAmount * tipPercentage / 100;
             double totalAmount = billAmount + tipAmount;
-            
-            // display the total
-            calculate.setTotal(totalAmount);// set total in calculate object 
 
-            //totalField.setText(String.format("$%.2f", totalAmount));
+            // display the total
+            calculate.setTotal(totalAmount);// set total in calculate object
+
+            // totalField.setText(String.format("$%.2f", totalAmount));
             totalField.setText(String.format("$%.2f", totalAmount));
-    
 
             /// Image element
             imageview = new ImageView("https://itxdesign.com/wp-content/uploads/2016/05/6072965_s-300x251.jpg");
@@ -135,9 +124,19 @@ public class CalPane extends GridPane {
             imageview.setPreserveRatio(true);
 
         } catch (NumberFormatException e) {
-            // ignore invalid input
+            throw new CalPane.NumberFormatException();
+            //billLabel.setText("Error: Enter a numeric value");
+            // // display error in tip field
+            // tipField.setText("Error: Enter a numeric value");
 
         }
 
+    }
+
+    public class NumberFormatException extends Exception {
+
+        public NumberFormatException() {
+            super("Error: Enter a numeric value");
+        }
     }
 }
